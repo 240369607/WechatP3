3d15abf86d8e689e18c132c90fca63e44796503d
DELL <DELL@hello>
2019-03-31 12:23:19 +0800

    8008

---
+++ src/main/java/com/zjj/wechat3/Wechat3Application.java
@@ -0,0 +1,13 @@
+package com.zjj.wechat3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wechat3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wechat3Application.class, args);
+	}
+
+}
